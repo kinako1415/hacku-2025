6054e0adcb8582608614f51115a52bf90a4d8cb3
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2025-09-17 21:13:47 +0900

    home画面レイアウト完成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,33 @@
 import Link from 'next/link';
 import styles from './page.module.scss';
 import Card from '@/components/layout/card';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <div className={styles.homePage}>
-      <Card
-        title="測定"
-        description={'AIカメラで手首と\n母指の可動域を正確に測定'}
-        isBlue={false}
+      <Image
+        src="homeDescription.svg"
+        alt="Top Illustration"
+        width={1060}
+        height={220}
       />
+      <div className={styles.cardWrapper}>
+        <Card
+          title="測定"
+          description={'AIカメラで手首と\n母指の可動域を正確に測定'}
+        />
+        <Card
+          title="進捗確認"
+          description={'測定データの推移と\n詳細な統計情報を表示'}
+          isBlue={false}
+        />
+        <Card
+          title="記録管理"
+          description={'日々のリハビリ記録を\nカレンダーで管理'}
+          isBlue={false}
+        />
+      </div>
     </div>
   );
 }
