d561433c547558bb1597efe304b89a3bb6124e53
kinako1415 <Owata09@outlook.jp>
2025-12-07 11:52:38 +0900

    feat: 動画ファイルのモジュール型定義を追加し、tsconfig.jsonにインクルード

---
+++ video.d.ts
@@ -0,0 +1,15 @@
+// 動画ファイルのモジュール型定義
+declare module '*.mp4' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.webm' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.ogg' {
+  const src: string;
+  export default src;
+}
